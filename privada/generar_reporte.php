e270ef68e996accb3fdd206033f238e19c9e6989
Alfredo Sanchez Nava <asanchez47@ucol.mx>
2023-05-25 16:56:22 -0600

    Modifica el gitignore

--- privada/generar_reporte.php
+++ privada/generar_reporte.php
@@ -4,76 +4,48 @@ include '../db_conn.php';
 require_once("login.php");
 // Librería de PhpSpreadsheet desde la CDN
 
-use PhpSpreadsheet\src\PhpSpreadsheet;
-use PhpSpreadsheet\src\PhpSpreadsheet\Writer\Xlsx;
-
 if (isset($_POST['generarReporte'])) {
+    if ($saml->isAuthenticated()) {
+        $atributos = $saml->getAttributes();
+    }
+    
+    //Obtener información número de cuenta del usuario
+    $nocuenta = $atributos["uCuenta"][0];
 
     // Obtener los datos del formulario
     $actividad = $_POST['actividadExcel'];
     $fechaInicio = $_POST['inicioExcel'];
     $fechaFin = $_POST['finExcel'];
 
-    // Validar si se seleccionó una actividad
-    if ($actividad == 0) {
-        echo "Por favor, selecciona una actividad.";
+    // Validar si se relleno todos los campos 
+    if ($actividad == 0 && empty($fechaInicio) && empty($fechaFin)) {
+        echo "Favor de rellenar todos los campos";
         exit;
-    }
-
-    // Crear el objeto Spreadsheet
-    $spreadsheet = new Spreadsheet();
-    $sheet = $spreadsheet->getActiveSheet();
-
-    // Configurar el encabezado
-    $sheet->setCellValue('A1', 'Actividad');
-    $sheet->setCellValue('B1', 'Fecha de inicio');
-    $sheet->setCellValue('C1', 'Fecha de fin');
+    } 
 
     // Realizar la consulta y obtener los datos de las tareas
-    $sql = "SELECT actividad, fecha_inicio, fecha_fin FROM tareas WHERE actividad = '$actividad'";
-
-    // Añadir condiciones adicionales si se seleccionaron fechas
-    if (!empty($fechaInicio)) {
-        $sql .= " AND fecha_inicio >= '$fechaInicio'";
-    }
-
-    if (!empty($fechaFin)) {
-        $sql .= " AND fecha_fin <= '$fechaFin'";
-    }
+    $sql = "SELECT * FROM tareas WHERE num_cuenta = '$nocuenta' AND actividad = '$actividad' AND fecha > '$fechaInicio' AND fecha < '$fechaFin'";
 
     // Ejecutar la consulta
     $result = $conn->query($sql);
 
     // Verificar si se obtuvieron resultados
     if ($result->num_rows > 0) {
-        // Iterar sobre los resultados y añadirlos al archivo Excel
-        $rowNumber = 2;
+
+        // Iterar resultados        
         while ($row = $result->fetch_assoc()) {
-            $sheet->setCellValue('A' . $rowNumber, $row['actividad']);
-            $sheet->setCellValue('B' . $rowNumber, $row['fecha_inicio']);
-            $sheet->setCellValue('C' . $rowNumber, $row['fecha_fin']);
-            $rowNumber++;
+            //  Crear registro
+
         }
 
-        // Crear el archivo Excel
-        $writer = new Xlsx($spreadsheet);
-        $fileName = 'reporte_excel.xlsx';
-        $writer->save($fileName);
 
-        // Descargar el archivo Excel al navegador
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="' . $fileName . '"');
-        header('Cache-Control: max-age=0');
-
-        $file = fopen($fileName, 'rb');
-        fpassthru($file);
-        fclose($file);
-
-        // Eliminar el archivo temporal
-        unlink($fileName);
+        
     } else {
         echo "No se encontraron tareas que cumplan los criterios de búsqueda.";
     }
+
+    header("Content-Type: application/vnd.ms-excel; charset=iso-8859-1");
+    header("Content-Disposition: attachment; filename='Reporte de usuario".$nocuenta.".xls'");
 }
 
 
